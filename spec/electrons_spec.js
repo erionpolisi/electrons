0fd0e1688819998596d31b598927f68efd7d6657
erionpolisi <er.polisi@gmail.com>
2025-06-16 18:29:16 +0200

    RED: Test-1

--- spec/electrons_spec.js
+++ spec/electrons_spec.js
@@ -1,1 +1,5 @@
-
+describe("Electrons around the cores", function() {
+  it("should return 'fehlerhafte Eingabe' for empty string input", function() {
+    expect(calculateElectrons("")).toBe("fehlerhafte Eingabe");
+  });
+});
